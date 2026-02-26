2ac1f687c46674c4ed11d29ac2508f9c108f792e
Adhvaithalakshmi <adhvaithalakshmi1@gmail.com>
2026-02-26 15:52:06 +0530

    UC3

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Palindrome {
+    static void main() {
+        String original ="madam";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println(original + "is a palindrome.");
+        } else {
+            System.out.println(original + "is not a palindrome.");
+        }
+    }
 }
