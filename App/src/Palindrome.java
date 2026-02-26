0db3071fd4aa7dde276584c3e3edcdefe4aba529
Adhvaithalakshmi <adhvaithalakshmi1@gmail.com>
2026-02-26 16:27:00 +0530

    UC4

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,24 +1,24 @@
+import java.util.Stack;
 public class Palindrome {
     static void main() {
-   String str="madam";
-   char[] arr=str.toCharArray();
-   int start=0;
-   int end=arr.length-1;
-   boolean isPalindrome =true;
-   while(start<end)
-   {
-       if(arr[start] != arr[end])
-       {
-           isPalindrome =false;
-           break;
-       }
-       start++;
-       end--;
-   }
-   if(isPalindrome) {
-       System.out.println(str + "is a palindrome.");
-   } else{
-       System.out.println(str + "is not a palindrome.");
+        String str="madam";
+        Stack<Character>stack =new Stack<>();
+        for(int i=0;i<str.length();i++) {
+            stack.push(str.charAt(i));
+        }
+        boolean isPalindrome=true;
+        for(int i=0;i<str.length();i++){
+            char poppedChar = stack.pop();
+            if (str.charAt(i) != poppedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome) {
+            System.out.println(str + "is a palindrome.");
+        }
+        else{
+            System.out.println(str + "is not a palindrome.");
    }
    }
     }
