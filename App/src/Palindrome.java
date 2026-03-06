0a1497132d5a08d82b117023dc7801b65520fe87
Adhvaithalakshmi <adhvaithalakshmi1@gmail.com>
2026-03-06 14:10:57 +0530

    UC6

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,24 +1,81 @@
-import java.util.Stack;
 public class Palindrome {
-    static void main() {
-        String str="madam";
-        Stack<Character>stack =new Stack<>();
-        for(int i=0;i<str.length();i++) {
-            stack.push(str.charAt(i));
+    static class Stack
+    {
+        char[] arr;
+        int top;
+        int size;
+        Stack(int n) {
+            arr = new char[n];
+            size = n;
+            top = -1;
         }
-        boolean isPalindrome=true;
-        for(int i=0;i<str.length();i++){
-            char poppedChar = stack.pop();
-            if (str.charAt(i) != poppedChar) {
-                isPalindrome = false;
-                break;
+        void push(char ch) {
+            if (top < size - 1) {
+                arr[++top] = ch;
             }
         }
-        if(isPalindrome) {
-            System.out.println(str + "is a palindrome.");
+        char pop()
+        {
+            if(top>=0) {
+                return arr[top--];
+            }
+            return '\0';
+            }
         }
-        else{
-            System.out.println(str + "is not a palindrome.");
-   }
-   }
-    }
+        static class Queue {
+            char[] arr;
+            int front, rear, size;
+
+            Queue(int n) {
+                arr = new char[n];
+                size = n;
+                front = 0;
+                rear = -1;
+            }
+
+            void enqueue(char ch) {
+                if (rear < size - 1) {
+                    arr[++rear] = ch;
+                }
+            }
+
+            char dequeue() {
+                if (front <= rear) {
+                    return arr[front++];
+                }
+                return '\0';
+            }
+        }
+            public static void main(String[] args) {
+
+            String str = "madam";
+
+            Stack stack = new Stack(str.length());
+            Queue queue = new Queue(str.length());
+
+            // Push into stack & Enqueue into queue
+            for (int i = 0; i < str.length(); i++) {
+                stack.push(str.charAt(i));
+                queue.enqueue(str.charAt(i));
+            }
+
+            boolean isPalindrome = true;
+
+            // Compare dequeue (FIFO) vs pop (LIFO)
+            for (int i = 0; i < str.length(); i++) {
+                char fromQueue = queue.dequeue();
+                char fromStack = stack.pop();
+
+                if (fromQueue != fromStack) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println(str + " is a Palindrome.");
+            } else {
+                System.out.println(str + " is NOT a Palindrome.");
+            }
+        }
+        }
